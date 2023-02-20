b7c2b19939d6c54ddb4cc48494ffbd87859c52b7
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-20 22:26:15 +0530

    Getting device location
    on clicking the "Get device location" btn,
    it will first ask for permission,
    if permission given we fetch its co-ordinates
    else display the error

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const container = document.querySelector(".container"),
   inputPart = document.querySelector(".input"),
   infoTxt = document.querySelector(".info-txt"),
-  inputField = document.querySelector("input");
+  inputField = document.querySelector("input"),
+  locationBtn = document.querySelector("button");
 const myapi = "422b97949e2d76a193e34a18e39f26b3";
+
 inputField.addEventListener("keyup", (e) => {
   // if user pressed enter btn and input value is not empty
   if (e.key === "Enter" && inputField.value != "") requestApi(inputField.value);
 });
+
+locationBtn.addEventListener("click", () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(onSuccess, onFailure);
+  } else {
+    alert("Your browser does not support geolocation api");
+  }
+});
+
+onSuccess = (position) => {
+  // If request allowed we get the coordinates
+  const { latitude, longitude } = position.coords;
+  console.log(latitude, longitude);
+};
+
+onFailure = (error) => {
+  // If request denied display error message
+  infoTxt.textContent = error.message;
+  infoTxt.classList.add("error");
+};
 
 requestApi = (city) => {
   let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myapi}`;
